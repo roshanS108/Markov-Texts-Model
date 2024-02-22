d711529fa0138660d75966d7916b479df997e446
= <roshanshoti100@gmail.com>
2024-02-22 13:02:08 -0500

    first commit

---
+++ src/main/java/com/Markovcharactermodel/MarkovCharacterModelApplication.java
@@ -0,0 +1,13 @@
+package com.Markovcharactermodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkovCharacterModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkovCharacterModelApplication.class, args);
+	}
+
+}
