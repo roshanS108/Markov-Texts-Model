9bcbb50c83e978ee62416dff76c2b12f0a341de8
= <roshanshoti100@gmail.com>
2024-02-25 13:42:01 -0500

    extending abstractmarkovmodel class properties, deleting all the current existing methods, adding toString method for better description of the class

--- src/main/java/com/Markovcharactermodel/markov/MarkovModel.java
+++ src/main/java/com/Markovcharactermodel/markov/MarkovModel.java
@@ -1,31 +1,15 @@
 package com.Markovcharactermodel.markov;
-
-import java.util.Random;
+import com.Markovcharactermodel.models.AbstractMarkovModel;
 import java.util.ArrayList;
-
-public class MarkovModel {
-    private String myText;
-    private Random myRandom;
+public class MarkovModel extends AbstractMarkovModel {
     private int n;
-
     public MarkovModel(int n) {
-        myRandom = new Random();
         this.n = n;
     }
-
-    public void setRandom(int seed){
-        myRandom = new Random(seed);
-    }
-
-    public void setTraining(String s){
-        myText = s.trim();
-    }
-
     public String getRandomText(int numChars){
         if (myText == null){
             return "";
         }
-
         StringBuilder sb = new StringBuilder();
         int index = myRandom.nextInt(myText.length() - n);
         String key = myText.substring(index, index + n);
@@ -43,25 +27,9 @@ public class MarkovModel {
             sb.append(next);
             key = key.substring(1) + next;
         }
-
         return sb.toString();
     }
-
-    public ArrayList<String> getFollows(String key) {
-        ArrayList<String> result = new ArrayList<String>();
-        int startPos = 0;
-
-        while(true) {
-            int index = myText.indexOf(key, startPos);
-
-            if (index == -1 || index == myText.length() - key.length()) {
-                break;
-            }
-
-            result.add(myText.substring(index + key.length(), index + key.length() + 1));
-            startPos = index + key.length();
-        }
-
-        return result;
+    public String toString() {
+        return "MarkovModel of order " + n + ".";
     }
 }
