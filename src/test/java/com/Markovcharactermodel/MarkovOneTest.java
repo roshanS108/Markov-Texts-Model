2b2c9a4a98494d194858dd304e318ca1589de931
= <roshanshoti100@gmail.com>
2024-02-27 18:15:20 -0500

    testing getFollowsWithSingleChar, and it works successfully

--- src/test/java/com/Markovcharactermodel/MarkovOneTest.java
+++ src/test/java/com/Markovcharactermodel/MarkovOneTest.java
@@ -1,18 +1,36 @@
 package com.Markovcharactermodel;
 import com.Markovcharactermodel.markov.MarkovOne;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MarkovOneTest {
     @Test
-    public void testGetRandomText(){
-
+    public void testGetRandomTextFromFile() throws IOException {
+        String testData = new String(Files.readAllBytes(Paths.get("data/alice.txt")));
         MarkovOne markovOne = new MarkovOne();
+        markovOne.setTraining(testData);
+        String randomText = markovOne.getRandomText(500);
+        assertNotNull(randomText);
+        assertEquals(500, randomText.length());
+    }
 
-        markovOne.setTraining("this is a test yes this is a test.");
+    @Test
+    public void testGetFollowsWithSingleChar() {
+        MarkovOne markovOne = new MarkovOne();
+        markovOne.setTraining("abracadabra");
+        //b, c, d, r
 
-        String result = markovOne.getRandomText(15);
-        assertEquals(15, result.length());
+        // Test the key "a"
+        List<String> followsA = markovOne.getFollows("a");
+        assertEquals(List.of("b", "c", "d", "b"), followsA);
+
+
     }
     @Test
     public void testGetRandomTextContainsOnlyCharacters() {
@@ -23,7 +41,6 @@ public class MarkovOneTest {
         String result = markovOne.getRandomText(15); // Generate random text of length 15
         assertTrue(result.matches("^[a-zA-Z]+$"), "The text does not contain any numbers"); // Check if the result contains only characters
     }
-
     @Test
     public void testGetFollows(){
 
