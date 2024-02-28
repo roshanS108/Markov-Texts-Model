b083bdb555ec9bc6d36265eb5bc2a0acefe68878
= <roshanshoti100@gmail.com>
2024-02-27 19:26:09 -0500

    updating

--- src/test/java/com/Markovcharactermodel/MarkovZeroTest.java
+++ src/test/java/com/Markovcharactermodel/MarkovZeroTest.java
@@ -1,17 +1,9 @@
 package com.Markovcharactermodel;
-import com.Markovcharactermodel.markov.MarkovOne;
 import com.Markovcharactermodel.markov.MarkovZero;
-import edu.duke.FileResource;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import static org.junit.jupiter.api.Assertions.*;
-public class MarkovZeroTest {
+public class MarkovZeroTest{
     // Testing for generating random text of the specified length
-
 
     @Test
     public void testToString(){
@@ -19,13 +11,7 @@ public class MarkovZeroTest {
         assertEquals("MarkovModel of order 0.", markovZero.toString());
     }
 
-    @Test
-    public void testGetRandomTextFromFile() throws IOException {
-        String testData = new String(Files.readAllBytes(Paths.get("data/alice.txt")));
-        MarkovOne markovOne = new MarkovOne();
-        markovOne.setTraining(testData);
-        String randomText = markovOne.getRandomText(500);
-        assertNotNull(randomText);
-        assertEquals(500, randomText.length());
-    }
+
+
+
 }
