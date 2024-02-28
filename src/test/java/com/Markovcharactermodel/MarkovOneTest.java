c528c20425f92ad22352c7192335bc44ff74b5e2
= <roshanshoti100@gmail.com>
2024-02-27 19:24:03 -0500

    dynamically generating the key values, for testing different keys

--- src/test/java/com/Markovcharactermodel/MarkovOneTest.java
+++ src/test/java/com/Markovcharactermodel/MarkovOneTest.java
@@ -22,23 +22,29 @@ public class MarkovOneTest {
     }
 
     @Test
-    public void testGetFollowsWithSingleChar() {
+    public void testGetFollowsWithSingleChar() throws IOException {
+        String testData = new String(Files.readAllBytes(Paths.get("data/testing.txt")));
         MarkovOne markovOne = new MarkovOne();
-        markovOne.setTraining("abracadabra");
-
-        String key = "c";
+        markovOne.setTraining(testData);
+        String randomText = markovOne.getRandomText(5);
+        assertNotNull(randomText);
+        String key = markovOne.getKeyForTesting();
+        System.out.println("key is: " + key);
         List<String> expectedFollows = new ArrayList<>();
         //Iterate through input text
         for(int i = 0; i<markovOne.getMyText().length()-1; i++){
             if (markovOne.getMyText().substring(i, i+1).equals(key)){
-                //store that in expectedFollows
+                //store that data in expectedFollows
                 expectedFollows.add(markovOne.getMyText().substring(i+1, i+2));
             }
         }
         // Test the any key
         List<String> followsA = markovOne.getFollows(key);
+        for(String follows: followsA){
+            System.out.println(follows);
+        }
         assertEquals(expectedFollows, followsA);
-
+//        assertEquals(500, randomText.length());
 
     }
     @Test
@@ -50,12 +56,6 @@ public class MarkovOneTest {
         String result = markovOne.getRandomText(15); // Generate random text of length 15
         assertTrue(result.matches("^[a-zA-Z]+$"), "The text does not contain any numbers"); // Check if the result contains only characters
     }
-    @Test
-    public void testGetFollows(){
 
-
-
-
-    }
 
 }
