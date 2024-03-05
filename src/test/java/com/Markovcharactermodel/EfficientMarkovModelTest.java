a042e7e11065f00f7f3ed0385ed3ee2f876402e0
= <roshanshoti100@gmail.com>
2024-03-05 17:52:45 -0500

    deleting unnecesasry dependencies, -->jupiter-engine,jupiter-api, as it comes with spring

--- src/test/java/com/Markovcharactermodel/EfficientMarkovModelTest.java
+++ src/test/java/com/Markovcharactermodel/EfficientMarkovModelTest.java
@@ -1,5 +1,4 @@
 package com.Markovcharactermodel;
-
 import com.Markovcharactermodel.efficient.EfficientMarkovModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,24 +6,23 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 public class EfficientMarkovModelTest {
-
     private EfficientMarkovModel efficientMarkovModel;
-
     @BeforeEach
     void setUp(){
         efficientMarkovModel = new EfficientMarkovModel(2);
     }
-
     /**
      * Test case to verify the constructor of EfficientMarkovModel class
      * Checks if the markov model is initialized correct with n-->2 parameter.
      */
     @ParameterizedTest
     @ValueSource(ints = {1,2,3,4})
-    void testConstructor(){
-        assertEquals(2, efficientMarkovModel.getN());
+    void testConstructor(int n){
+        //creating instance of EfficientMarkovModel instead of relying on setUp method, as it is also good practice
+        //to create instance inside parameterized test method
+        EfficientMarkovModel efficientMarkovModel = new EfficientMarkovModel(n);
+        assertEquals(n, efficientMarkovModel.getN());
     }
 
     /**
