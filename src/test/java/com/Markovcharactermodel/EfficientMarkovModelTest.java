9d8e1110a78237e035c40961940480cd73c804d6
= <roshanshoti100@gmail.com>
2024-03-05 17:07:06 -0500

    testing if the constructor of the efficientmarkovmodel is initialized correctly

--- src/test/java/com/Markovcharactermodel/EfficientMarkovModelTest.java
+++ src/test/java/com/Markovcharactermodel/EfficientMarkovModelTest.java
@@ -1,4 +1,27 @@
 package com.Markovcharactermodel;
 
+import com.Markovcharactermodel.efficient.EfficientMarkovModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EfficientMarkovModelTest {
+
+    private EfficientMarkovModel efficientMarkovModel;
+
+    @BeforeEach
+    void setUp(){
+        efficientMarkovModel = new EfficientMarkovModel(2);
+    }
+
+    /**
+     * Test case to verify the constructor of EfficientMarkovModel class
+     * Checks if the markov model is initialized correct with n-->2 parameter.
+     */
+    @Test
+    void testConstructor(){
+        assertEquals(2, efficientMarkovModel.getN());
+    }
+
 }
