0955b324edb3b6240f2722c965336a8868e327cb
= <roshanshoti100@gmail.com>
2024-03-08 13:19:36 -0500

    markovword class to predict next word based on current word

--- src/main/java/com/Markovmodel/markov/MarkovWordOne.java
+++ src/main/java/com/Markovmodel/markov/MarkovWordOne.java
@@ -1,4 +1,85 @@
 package com.Markovmodel.markov;
 
-public class MarkovWordOne {
+import com.Markovmodel.interfaces.IMarkovModel;
+
+import java.util.*;
+
+public class MarkovWordOne implements IMarkovModel {
+    private String[] myText;
+    private Random myRandom;
+
+    public MarkovWordOne() {
+        myRandom = new Random();
+    }
+
+    public void setRandom(int seed) {
+        myRandom = new Random(seed);
+    }
+
+    public void setTraining(String text){
+        myText = text.split("\\s+");
+    }
+
+    public String getRandomText(int numWords){
+        StringBuilder sb = new StringBuilder();
+        int index = myRandom.nextInt(myText.length-1);  // random word to start with
+        String key = myText[index];
+        sb.append(key);
+        sb.append(" ");
+        for(int k=0; k < numWords-1; k++){
+            ArrayList<String> follows = getFollows(key);
+            if (follows.size() == 0) {
+                break;
+            }
+            index = myRandom.nextInt(follows.size());
+            String next = follows.get(index);
+            sb.append(next);
+            sb.append(" ");
+            key = next;
+        }
+
+        return sb.toString().trim();
+    }
+
+    private int indexOf(String[] words, String target, int start){
+        for(int k = start; k < words.length; k++){
+            if(words[k].equals(target)){
+                return k;
+            }
+        }
+        return -1;
+    }
+    private ArrayList<String> getFollows(String key) {
+        ArrayList<String> follows = new ArrayList<String>();
+        int pos = 0;
+        while (pos < myText.length) {
+            int start = indexOf(myText, key, pos);
+            if (start == -1) {
+                break;
+            }
+            if (start >= myText.length - 1) {
+                break;
+            }
+            String next = myText[start+1];
+            follows.add(next);
+            pos = start + 1;
+        }
+
+        return follows;
+    }
+    public void testIndexOf() {
+        String text = "this is just a test yes this is a simple test";
+        String[] words = text.split("\\s+");
+
+        System.out.println(indexOf(words, "this", 0));
+        System.out.println(indexOf(words, "this", 3));
+        System.out.println(indexOf(words, "frog", 0));
+
+    }
+
+    public static void main(String[] args) {
+        MarkovWordOne markovWordOne = new MarkovWordOne();
+        markovWordOne.testIndexOf();
+    }
+
 }
